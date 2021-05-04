739a8562612ee28982bf578d9eae9d8cf10dda9d
Nitay Caspi <NitayCaspi12@gmail.com>
2021-05-05 00:26:27 +0300

    add an "add to cart" functionallity

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
 
               <router-link to="/cart" class="button is-success">
               <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-              <span>Cart</span>
+              <span>Cart ({{cartTotalLenght}})</span>
               </router-link>
             </div>
           </div>
@@ -44,6 +44,26 @@ export default {
   data(){
     return{
       showMobileMenu: false,
+      cart: {
+        items: []
+      }
+    }
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+  },
+  mounted(){
+    this.cart = this.$store.state.cart
+  },
+  computed: {
+    cartTotalLenght() {
+      let totalLenght = 0
+
+      for (let i = 0; i < this.cart.items.length; i++) {
+        totalLenght += this.cart.items[i].quantity
+      }
+
+      return totalLenght
     }
   }
 }

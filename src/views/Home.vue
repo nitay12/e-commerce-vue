b8162166ec8406f810ddd5e962fcee818f0972b4
Nitay Caspi <NitayCaspi12@gmail.com>
2021-05-09 01:14:52 +0300

    add ProductBox component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,29 +16,19 @@
       <h2 class="is-size-2 has-text-centered">Latest products</h2>
     
     </div>
-  <div 
-    class="column is-3"
-    v-for="product in latestProducts"
-    v-bind:key=product.id
-  >
-  
-    <div class="box">
-      <figure class="image mb-4">
-        <img :src="product.get_thumbnail">
-      </figure>
-      <h3 class="is-size-4">{{product.name}}</h3>
-      <p class="is-size-6 has-text-gray">${{product.price}}</p>
-
-      <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
-      </div>
-        
-      </div>
+      <ProductBox
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios"
+
+import ProductBox from '@/components/ProductBox'
+
 export default {
   name: 'Home',
   data() {
@@ -47,11 +37,12 @@ export default {
     }
   },
   components: {
+    ProductBox
   },
   mounted() {
     this.getLatestProducts()
 
-    document.title = 'Home | Django|'
+    document.title = 'Home | Django'
   },
   methods: {
     getLatestProducts() {
@@ -68,11 +59,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-  .image {
-    margin-top:-1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
-</style>

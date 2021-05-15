c2c22b74c054d80bca7352d9f13f54872a06b174
Nitay Caspi <NitayCaspi12@gmail.com>
2021-05-15 22:16:12 +0300

    add my account + checkout path

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import store from '../store'
 import Home from '../views/Home.vue'
 
 import Product from '../views/Product.vue'
@@ -7,6 +9,8 @@ import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
 
 const routes = [
   {
@@ -33,6 +37,14 @@ const routes = [
     component: LogIn
   },
   {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
     path: '/search',
     name: 'Search',
     component: Search
@@ -41,6 +53,14 @@ const routes = [
     path: '/cart',
     name: 'Cart',
     component: Cart
+  },
+  {
+    path: '/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
+    meta: {
+      requireLogin: true
+    }
   },
   {
     path: '/:category_slug/:product_slug',
@@ -57,6 +77,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({name: 'LogIn', query: {to: to.path} })
+  } else {
+    next()
+  }
 })
 
 export default router

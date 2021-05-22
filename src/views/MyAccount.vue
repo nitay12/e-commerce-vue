adf1bdf77728c938f0a00f0b2250c2f3e73ee3ac
Nitay Caspi <NitayCaspi12@gmail.com>
2021-05-23 01:29:34 +0300

    add order summary

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -8,14 +8,41 @@
             <div class="column is-12">
                 <button class="button is-danger" @click="logout()">Log out</button>
             </div>
+
+            <hr>
+
+            <div class="column is-12">
+                <h2 class="subtitle">My Orders</h2>
+
+                <OrderSummary
+                    v-for="order in orders"
+                    v-bind:key="order.id"
+                    v-bind:order="order"/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+
+import OrderSummary from '@/components/OrderSummary.vue'
+
 export default {
     name: 'MyAccount',
+    components: {
+        OrderSummary
+    },
+    data() {
+        return {
+            orders: []
+        }
+    },
+    mounted() {
+        document.title = 'My account | Django'
+
+        this.getMyOrders()
+    },
     methods: {
         logout() {
             axios.defaults.headers.common['Authorization'] = ""
@@ -27,6 +54,19 @@ export default {
             this.$store.commit('removeToken')
 
             this.$router.push('/')
+        },
+        async getMyOrders() {
+            this.$store.commit('setIsLoading', true)
+            await axios
+                .get('/api/v1/orders/')
+                .then(response => {
+                    console.log(response)
+                    this.orders = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+            this.$store.commit('setIsLoading', false)
         }
     }
 }

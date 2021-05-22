4c3afb7e7f9d23ec757a0ec34355b38b34192105
Nitay Caspi <NitayCaspi12@gmail.com>
2021-05-23 01:27:40 +0300

    fix typo

--- src/App.vue
+++ src/App.vue
@@ -87,7 +87,7 @@ export default {
     const token = this.$store.state.token
 
     if (token) {
-      axios.defaults.headers.common['Authorization'] = 'Token' + token
+      axios.defaults.headers.common['Authorization'] = 'Token ' + token
     } else {
       axios.defaults.headers.common['Authorization'] = ""
     }
